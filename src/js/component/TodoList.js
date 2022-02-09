0147fbb629434255ab542ce081939ff0e6299ad9
cnvorous <cnvorous@gmail.com>
2022-02-09 18:19:13 +0000

    Made notes about the functions and actions

--- src/js/component/TodoList.js
+++ src/js/component/TodoList.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 export const TodoList = () => {
 	const [inputTask, setInputTask] = useState("");
-	const [taskList, setTaskList] = useState([]);
+	const [taskList, setTaskList] = useState([]); // SetTaskList changes value of tasklist
 
 	// (put the function in the code)
 	// function onChange(e) {
@@ -12,13 +12,13 @@ export const TodoList = () => {
 
 	const saveTask = (e) => {
 		if (e.keyCode == 13) {
-			setTaskList([...taskList, inputTask]);
-			setInputTask("");
+			setTaskList([...taskList, inputTask]); // this is updating value of tasklist
+			setInputTask(""); // this is clearing out the input text field after enter
 		}
 	};
 
 	const removeTask = (index) => {
-		setTaskList(taskList.filter((tasktToRemove, i) => i != index));
+		setTaskList(taskList.filter((tasktToRemove, i) => i != index)); //filter is higher order function. makes new array w/out that one index
 	};
 	return (
 		<div className="todo-box">
@@ -26,19 +26,24 @@ export const TodoList = () => {
 			<input
 				className="center-block"
 				type="text"
-				onChange={(e) => setInputTask(e.target.value)}
-				value={inputTask}
-				onKeyUp={(e) => saveTask(e)}
+				onChange={(e) => setInputTask(e.target.value)} // this saves the info user input
+				value={inputTask} ////////////////////////////// this displays/onlys shows whats typed in input (currently) from the user in variable
+				onKeyUp={(e) => saveTask(e)} // passing e(onkeyupevent) to the function saveTask
 				//onChange={onChange} if used function above to call
 			/>
 			<ul>
 				{taskList.map((task, index) => {
+					// map function passes a function inisde of it (always pass item 1st then index)
 					return (
 						<li className="list" key={index}>
+							{" "}
+							{/*must have key when using map function*/}
 							{task}
 							<span
 								className="delete-icon"
 								onClick={() => removeTask(index)}>
+								{" "}
+								{/*all we care about is the index to remove task not the event itself. we are not trying to save any value by the onclick*/}
 								<i className="fas fa-trash"></i>
 							</span>
 						</li>
